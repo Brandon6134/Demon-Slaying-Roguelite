cb57c320abe0d5f82bd8cfe18f9de709b7371930
Brandon Kong <brandonkong@rogers.com>
2026-03-31 20:14:38 -0400

    added CameraControl to make camera follow player

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        Vector3 playerPos = new Vector3(player.transform.position.x,player.transform.position.y,-10f);
+        transform.position = playerPos;
+    }
+}
